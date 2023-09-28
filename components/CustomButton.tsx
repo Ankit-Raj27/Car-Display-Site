b17c4ca69dae514ec45bec93dddc39ec77537ae2
Ankit-Raj27 <ankitrajk27@gmail.com>
2023-09-28 19:25:59 +0530

    [update] added car catalogue

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,25 +1,37 @@
-"use client"; 
-import React from 'react'
-import Image from 'next/image';
-import { CustomButtonProps } from '@/types';
+"use client";
+import React from "react";
+import Image from "next/image";
+import { CustomButtonProps } from "@/types";
 
-const CustomButton = ({title,containerStyles,handleClick,btnType}: CustomButtonProps) => {
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+  textStyle,
+  rightIcon,
+  isDisabled,
+}: CustomButtonProps) => {
   return (
-    <div>
-        <button 
-            disabled={false}
-            type = {btnType || "button"}
-            className={`custom-btn ${containerStyles}`  }
-            onClick={(handleClick) => {
+    <button
+      disabled={false}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={(handleClick) => {}}
+    >
+      <span className={`flex-1 ${textStyle}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6 ">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
+    </button>
+  );
+};
 
-            }}>
-                <span className={'flex-1'}>
-                    {title}
-                </span>
-            
-        </button>
-    </div>
-  )
-}
-
-export default CustomButton
+export default CustomButton;
