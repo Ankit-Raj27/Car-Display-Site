3041de992415cd3665294eb1fb874013b683a85c
Ankit-Raj27 <ankitrajk27@gmail.com>
2023-09-28 20:08:26 +0530

    [update]

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { CarProps } from '@/types';
+import Image from 'next/image';
+import {Fragment} from 'react';
+import { Dialog,Transition } from '@headlessui/react';
 
-const CarDetails = () => {
+
+
+import React from 'react'
+interface CarDetailsProps{
+    isOpen : boolean;
+    closeModal : () => void;
+    car : CarProps;
+}
+
+const CarDetails = ({isOpen,closeModal,car}:CarDetailsProps) => {
   return (
     <div>
-      
+    
     </div>
   )
 }
