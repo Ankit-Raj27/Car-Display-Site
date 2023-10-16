5beefcc092a2311eb0dc0d7cd4cd48616c6564dc
Ankit-Raj27 <ankitrajk27@gmail.com>
2023-10-16 20:24:29 +0530

    [update] CarDeatails

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,23 +1,56 @@
 "use client";
-import {useState} from 'react';
-import { SearchManufacturer } from './';
+import { useState } from "react";
+import { SearchManufacturer } from "./";
+import Image from "next/image";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src="./magnifying-glass.svg"
+      alt="magnifying glass"
+      width={40}
+      height={40}
+      className="object-contained"
+    />
+  </button>
+);
+
 const SearchBar = () => {
-    const [manufacturer,setmanufacturer] = useState("");
+  const [manufacturer, setmanufacturer] = useState("");
+  const [model, setModel] = useState("");
 
-    const handleSearch =() => {
-
-    }
+  const handleSearch = () => {};
 
   return (
-    <form className="searchbar" onSubmit={handleSearch}> 
-        <div className = "searchbar__items">
-            <SearchManufacturer 
-                manufacturer = {manufacturer}
-                setmanufacturer = {setmanufacturer}
-            />
-        </div>
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__items">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setmanufacturer={setmanufacturer}
+        />
+        <SearchButton otherClasses={"sm:hidden"} />
+      </div>
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          alt="model-icon"
+          className="absolute w-[20px] h-[20px] ml-4"
+        />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan"
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden"/>
+      </div>
+        <SearchButton otherClasses="max-sm:hidden"/>
     </form>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
--- utils/index.ts
+++ utils/index.ts
@@ -5,7 +5,7 @@ export async function fetchCars(){
             'X-RapidAPI-Key': '484aee40a3msh3de3d892007e0b5p17b34cjsncd01a63afae2',
             'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=r8',
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=bmw',
     {headers:headers,});
 
     const result = await response.json();
